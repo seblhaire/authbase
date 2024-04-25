d685fd9d9575ea70c840422e99a69d934f3ad82a
Sébastien L'haire <sebastien@lhaire.org>
2024-04-25 21:57:53 +0200

    new structure

--- config/authbase.php
+++ config/authbase.php
@@ -3,4 +3,11 @@
 return [
     'resetpasswordnotification' => '',
     'createpasswordnotification' => '',
+    'roles' => [
+        'administrator',
+        'standard_user',
+    ],
+    'profiles' => [
+    //'table_max_element',
+    ],
 ];
--- src/Providers/AuthbaseServiceProvider.php
+++ src/Providers/AuthbaseServiceProvider.php
@@ -20,6 +20,9 @@ class AuthbaseServiceProvider extends ServiceProvider {
             __DIR__ . '/../../config/authbase.php' => config_path('authbase.php'),
             __DIR__ . '/../../resources/views/public' => resource_path('views/vendor/authbase/public'),
             __DIR__ . '/../../lang' => app()->langPath('vendor/authbase'),
+            __DIR__ . '/../../database/migrations/0000_00_00_000000_baseauth_tables.php' => 
+                    database_path('migrations/' . now()->format('Y_m_d_His') . '_baseauth_tables.php'),
+            __DIR__ . '/../../database/seeders/UsersTableSeeder.php' => database_path('seeders/UsersTableSeeder.php'),
         ]);
         //$this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
         //$this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
