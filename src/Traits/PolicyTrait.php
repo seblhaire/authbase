a13f545ae385467dec87e69fefea065cbe30a880
Sébastien L'haire <sebastien@lhaire.org>
2024-05-09 11:52:32 +0200

    trait

--- src/Traits/PolicyTrait.php
+++ src/Traits/PolicyTrait.php
@@ -1,15 +1,14 @@
 <?php
 namespace Seblhaire\Authbase\Traits;
 
-use Seblhaire\Specialauth\Models\User; 
 
 trait PolicyTrait{
-    public function update(User $user, User $updateduser) {
+    public function update($user, $updateduser) {
         \Log::info($user->id . ' ' . $updateduser->id . ' ' . ($user->hasRole('administrator') ? 'admin': 'no'));
         return $user->hasRole('administrator') || $user->id === $updateduser->id;
     }
 
-    public function display(User $user, User $updateduser) {
+    public function display($user, $updateduser) {
         return $user->id === $updateduser->id;
     }
 }
