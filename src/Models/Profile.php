23a906044ffc0f09503eaf6463c70d0b99a02cfa
Sébastien L'haire <sebastien@lhaire.org>
2024-06-13 18:31:31 +0200

    models

--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -3,10 +3,11 @@
 namespace Seblhaire\Authbase\Models;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use Seblhaire\Authbase\Models\User;
 
 class Profile extends Eloquent {
 
     public function users() {
-        return $this->belongsToMany(\Seblhaire\Authbase\Models\User);
+        return $this->belongsToMany(User);
     }
 }
